735f891af8f0a5089fa12bbbcec110b802a06561
Biswamohan-creator <56292855+Biswamohan-creator@users.noreply.github.com>
2022-10-09 11:12:50 +0530

    first commit

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by CHINAM DWARIKANATH PATRA on 23/06/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
